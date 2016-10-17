592cf0c2c8c49787c3f4c647ccc34628280e794e
Narsa Reddy <narsareddy@users.noreply.github.com>
2016-10-16 23:41:23 -0700

    Create SQL-Only-Migration-Template.sql

---
+++ SQL-Only-Migration-Template.sql
@@ -0,0 +1,13 @@
+-- ************
+-- Database name: ASUCSSUP
+-- Database description: N/A
+-- ********** Description of SQL statement *************
+-- SQL to insert data into SRPCALCD's run control table.
+-- ***************
+ 
+ 
+--Number of rows inserted: 1
+--SQL to insert data into SRPCALCD's run control table.
+INSERT INTO PS_RUNCNTL_CALC (OPRID, RUN_CNTL_ID, INSTITUTION, ACAD_CAREER, EMPLID, STRM, MODEL_NBR, SWITCH1, SWITCH2, SWITCH3, SWITCH4) VALUES ('USER1', '1', 'ASU00', 'UGRD', '1234567890', '2141', '1', 'N', 'Y', 'N', 'N');
+ 
+-- END OF SCRIPT/STATEMENT
