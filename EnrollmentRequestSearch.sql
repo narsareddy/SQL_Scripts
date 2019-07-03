4700327df6eeaee0eeee64ee05cbe1f9bdb6365d
Narsa Reddy <narsareddy@users.noreply.github.com>
2019-07-03 10:52:05 -0700

    Update EnrollmentRequestSearch.sql

--- EnrollmentRequestSearch.sql
+++ EnrollmentRequestSearch.sql
@@ -1,9 +1,33 @@
-SELECT * 
-FROM PS_ENRL_REQ_HEADER 
-WHERE ENRL_REQUEST_ID = ''
-ORDER BY DTTM_STAMP_SEC DESC;
 
-SELECT * 
-FROM PS_ENRL_REQ_DETAIL
-WHERE EMPLID = ''
-ORDER BY DTTM_STAMP_SEC DESC;
+select * 
+from ps_enrl_req_header h
+where h.enrl_request_id = ''
+order by h.dttm_stamp_sec desc;
+
+select * 
+from ps_enrl_req_detail d
+where d.emplid = ''
+order by d.dttm_stamp_sec desc;
+
+select * 
+from ps_enrl_msg_log m
+where m.enrl_request_id = ''
+order by m.dttm_stamp_sec desc;
+
+select * 
+from ps_enrl_msg_parm p
+where p.enrl_request_id = ''
+order by p.dttm_stamp_sec desc;
+
+select * 
+from ps_enrl_req_header h, ps_enrl_req_detail d
+where d.emplid = ''
+and h.enrl_request_id = d.enrl_request_id
+order by d.dttm_stamp_sec desc;
+
+select * 
+from ps_enrl_req_detail d, ps_enrl_msg_log m--, ps_enrl_msg_parm p
+where d.emplid = ''
+and d.enrl_request_id = m.enrl_request_id
+and d.enrl_req_detl_seq = m.enrl_req_detl_seq
+order by d.dttm_stamp_sec desc;
